dc83901744b79bcda09658e7e77643cb326a0a39
uzulla <uzulla@himitsukichi.com>
2020-09-26 17:15:20 +0900

    Improved result clarity

--- src/amphp/co_http_client.php
+++ src/amphp/co_http_client.php
@@ -13,51 +13,15 @@ $time = microtime(true);
 
 $result = [];
 
+// async
 asyncCall(function () use (&$result){
     $client = HttpClientBuilder::buildDefault();
 
-    $uris = [
-        "https://speakerdeck.com/uzulla?1",
-        "https://speakerdeck.com/uzulla?2",
-        "https://speakerdeck.com/uzulla?3",
-        "https://speakerdeck.com/uzulla?4",
-        "https://speakerdeck.com/uzulla?5",
-    ];
+    $uris = [];
 
-    $requestHandler = static function (string $uri) use ($client): Generator {
-        /** @var Response $response */
-        $response = yield $client->request(new Request($uri));
-        return yield $response->getBody()->buffer();
-    };
-
-    try {
-        $promises = [];
-
-        foreach ($uris as $uri) {
-            $promises[$uri] = Amp\call($requestHandler, $uri);
-        }
-
-        $bodies = yield $promises;
-
-        foreach ($bodies as $uri => $body) {
-            echo "-";
-            $result[] = $uri . " - " . \strlen($body) . " bytes" . substr($body, 0, 30) . PHP_EOL;
-        }
-    } catch (HttpException $error) {
-        echo $error;
+    for ($i = 0; $i < 379; ++$i) {
+        $uris[] = "https://http2.akamai.com/demo/tile-$i.png";
     }
-});
-
-asyncCall(function () use (&$result){
-    $client = HttpClientBuilder::buildDefault();
-
-    $uris = [
-        "http://cfe.jp/?1",
-        "http://cfe.jp/?2",
-        "http://cfe.jp/?3",
-        "http://cfe.jp/?4",
-        "http://cfe.jp/?5",
-    ];
 
     $requestHandler = static function (string $uri) use ($client): Generator {
         /** @var Response $response */
@@ -83,14 +47,29 @@ asyncCall(function () use (&$result){
     }
 });
 
-asyncCall(function (){
-    yield new Delayed(2000);
+asyncCall(function (){ // just add another coroutine.
+    for ($i = 0; $i < 3; ++$i) {
+        echo "+";
+        yield new Delayed(100);
+    }
 });
 
-echo "registered" . (microtime(true) - $time) . "sec" . PHP_EOL;
+echo PHP_EOL. "loop registered" . (microtime(true) - $time) . "sec" . PHP_EOL;
 
 Loop::run(); // wait all
 
-var_dump($result);
+// var_dump($result); // result set
 
-echo "para finish" . (microtime(true) - $time) . "sec" . PHP_EOL;
+echo PHP_EOL. "para finish" . (microtime(true) - $time) . "sec" . PHP_EOL;
+
+// sync
+$time = microtime(true);
+
+for ($i = 0; $i < 379; ++$i) {
+    $uri = "https://http2.akamai.com/demo/tile-$i.png";
+    $body = file_get_contents($uri);
+    echo "."; //$uri . " - " . \strlen($body) . " bytes" . substr($body, 0, 30) . PHP_EOL;
+}
+
+echo "sync finish" . (microtime(true) - $time) . "sec" . PHP_EOL;
+
