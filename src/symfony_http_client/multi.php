b33efd602956fe6adb118e6e001348b56c83c5d9
uzulla <uzulla@himitsukichi.com>
2020-09-26 17:12:47 +0900

    Improved result clarity

--- src/symfony_http_client/multi.php
+++ src/symfony_http_client/multi.php
@@ -3,12 +3,13 @@
 use Symfony\Component\HttpClient\CurlHttpClient;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
-require_once (__DIR__."/vendor/autoload.php");
+require_once(__DIR__ . "/vendor/autoload.php");
 
 $client = new CurlHttpClient();
 
 $time = microtime(true);
 
+// パラレルテスト
 $responses = [];
 for ($i = 0; $i < 379; ++$i) {
     $uri = "https://http2.akamai.com/demo/tile-$i.png";
@@ -23,15 +24,18 @@ $i = 0;
 foreach ($responses as $response) {
     $i++;
     $content = $response->getContent();
-    echo "{$i} - length ". strlen($content).PHP_EOL;
+    echo '.'; // "{$i} - length ". strlen($content).PHP_EOL;
 }
 
-echo "use parallel" . (microtime(true) - $time) . "sec" . PHP_EOL;
+echo PHP_EOL . "use parallel " . (microtime(true) - $time) . "sec" . PHP_EOL;
+
+// パラレルしない直列テスト
+$time = microtime(true);
 
 for ($i = 0; $i < 379; ++$i) {
     $uri = "https://http2.akamai.com/demo/tile-$i.png";
     $content = file_get_contents($uri);
-    echo "{$i} - length ". strlen($content).PHP_EOL;
+    echo '.'; // "{$i} - length ". strlen($content).PHP_EOL;
 }
 
-echo "use sync" . (microtime(true) - $time) . "sec" . PHP_EOL;
+echo PHP_EOL . "use sync " . (microtime(true) - $time) . "sec" . PHP_EOL;
