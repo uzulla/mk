1c732b196c0e6108c5b6973bce52e77cb27d42b9
uzulla <uzulla@himitsukichi.com>
2020-09-27 18:27:00 +0900

    more reality.

--- src/Preload/test2/generate.php
+++ src/Preload/test2/generate.php
@@ -2,7 +2,11 @@
 
 // generate tons of files.
 for ($i = 0; $i < 10000; $i++) {
-    $code = '<?php '.PHP_EOL.'function cache_func_' . microtime(true)*10000 . '_' . $i . '(){ return [1,2,3];}' . PHP_EOL;
+    $code = '<?php '.PHP_EOL.'function cache_func_' . microtime(true)*10000 . '_' . $i . '(){ return 
+    '.
+    var_export(ini_get_all(), true)
+    .'
+    ;}' . PHP_EOL;
     file_put_contents(__DIR__ . "/store/" . microtime(true)*10000 . '_' . $i . ".php", $code);
 }
 
